034d41f137cb2fed3aac39551a0379d29eb91eb3
WalterKc <trabajosham@gmail.com>
2022-12-05 18:06:16 -0300

    primer commit
    mi version de la pokedex (a mejorar)

---
+++ cypress/e2e/pokedexTest.cy.js
@@ -0,0 +1,24 @@
+let arrayPokemons = document.querySelectorAll("#pokemon");
+describe("empty spec", () => {
+  it("passes", () => {
+    cy.visit("http://192.168.0.2:8080");
+    cy.get("#selectorPagina").select("2");
+    cy.wait(1000);
+    cy.get("#irAPagina").click();
+    cy.get("#fila #pokemon").contains("spearow");
+    cy.get("#fila #pokemon").first().click();
+    cy.wait(1000);
+    cy.get("#contendenorPokemones").should("have.attr", "hidden");
+    cy.get("#botonVolver").click();
+    cy.wait(1000);
+    cy.get("#pokemonSelecionado").should("have.attr", "hidden");
+    cy.get("#botonAnterior").click().should("have.attr", "hidden");
+    cy.wait(1000);
+    cy.get("#botonSiguiente").click();
+    cy.wait(1000);
+    cy.get("#selectorPagina").select("57");
+    cy.get("#irAPagina").click();
+    cy.wait(1000);
+    cy.get("#botonSiguiente").should("have.attr", "hidden");
+  });
+});
